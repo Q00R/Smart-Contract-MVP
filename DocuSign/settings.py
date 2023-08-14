c904b315e9af8a923bcd90f85feb2ee2e82051b6
Mahmouddd21 <mahmoudosama11.msed@gmail.com>
2023-08-14 13:16:00 +0300

    connect django to postgres

--- DocuSign/settings.py
+++ DocuSign/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'DocuSign.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'valify_internship',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
+        'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
 
