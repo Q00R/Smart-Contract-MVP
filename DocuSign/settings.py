f64d8164147c6eb61a32a7e88afb2ce1ffba62eb
Abdullah abooof <abdullah.abooof@gmail.com>
2023-08-14 12:32:03 +0300

    setup

--- DocuSign/settings.py
+++ DocuSign/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'uses.apps.UsersConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view , permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+#import meen .serializers w models     @api_view(['GET'])
+#@permission_classes([IsAuthenticated]) for jwt 
