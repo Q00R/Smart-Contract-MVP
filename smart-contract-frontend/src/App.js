8e48880a61f286a8cdfb666bbb36e4f107b0c5ae
EyadTamer999 <eyadhassan2002@gmail.com>
2023-08-20 01:01:56 +0300

    fixed packages, added navbar and home

--- smart-contract-frontend/src/App.js
+++ smart-contract-frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/index';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
