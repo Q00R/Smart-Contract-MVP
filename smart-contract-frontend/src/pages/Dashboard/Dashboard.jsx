732158f9131bb8edbbf0b23a5c2138fb040678ff
EyadTamer999 <eyadhassan2002@gmail.com>
2023-08-29 11:56:56 +0300

    idk kill me

--- smart-contract-frontend/src/pages/Dashboard/Dashboard.jsx
+++ smart-contract-frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import UploadPDF from '../../components/UploadPDF';
 
-
 const Dashboard = () => {
-
-
 
     return (
         <div className="flex flex-col">
-
             <div className="self-center">
                 <UploadPDF />
             </div>
-
         </div>
     );
 };
