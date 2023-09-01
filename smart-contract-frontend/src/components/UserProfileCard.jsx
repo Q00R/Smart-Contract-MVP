1e85edcf0e245356135eb4201b2ff2526a64f843
EyadTamer999 <eyadhassan2002@gmail.com>
2023-09-01 23:57:15 +0300

    user profile now looks decent

--- smart-contract-frontend/src/components/UserProfileCard.jsx
+++ smart-contract-frontend/src/components/UserProfileCard.jsx
@@ -1,41 +1,40 @@
 import React from 'react'
+import editIcon from '../Assets/EditIcon.png'
 
 const UserprofileCard = () => {
     return (
-        <div class="flex items-center h-screen w-full justify-center">
-
-            <div class="max-w-xl">
-                <div class="bg-base-200 shadow-xl rounded-lg py-3">
-                    <div class="p-2">
-                        <h3 class="text-center text-2xl text-neutral font-medium leading-8">John Doe</h3>
-                        <div class="text-center text-base-content text-lg font-semibold">
-                            <p>Web Developer</p>
-                        </div>
-                        <table class="text-xl my-3">
-                            <tbody>
-                                <tr>
-                                    <td class="px-2 py-2 text-base-content font-semibold">Phone</td>
-                                    <td class="px-2 py-2">+977 9955221114</td>
-                                </tr>
-                                <tr>
-                                    <td class="px-2 py-2 text-base-content font-semibold">Email</td>
-                                    <td class="px-2 py-2">john@exmaple.com</td>
-                                </tr>
-                                <tr>
-                                    <td class="px-2 py-2 text-base-content font-semibold">National ID</td>
-                                    <td class="px-2 py-2">3123131313</td>
-                                </tr>
-                            </tbody></table>
-
-                        <div class="text-center my-3">
-                            <a class="text-xl text-primary italic hover:text-primary-focus font-medium" href="#">View Profile</a>
-                        </div>
-
-                    </div>
-                </div>
+        <div class="flex flex-col items-center h-screen w-full justify-center">
+            <div class="bg-base-200 shadow-xl rounded-lg py-3">
+                <h3 class="text-center text-2xl text-base-content font-medium leading-8">John Doe</h3>
+                <table class="text-xl my-3 mx-5">
+                    <tbody>
+                        <tr>
+                            <td class="px-2 py-2 text-base-content font-semibold">Phone</td>
+                            <td class="px-2 py-2">+977 9955221114</td>
+                        </tr>
+                        <tr>
+                            <td class="px-2 py-2 text-base-content font-semibold">Email</td>
+                            <td class="px-2 py-2">john@exmaple.com</td>
+                        </tr>
+                        <tr>
+                            <td class="px-2 py-2 text-base-content font-semibold">National ID</td>
+                            <td class="px-2 py-2">3123131313</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button className='mt-12 btn-sm btn btn-outline btn-info'>Edit Account</button>
+                            </td>
+                            <td>
+                                <button className='mx-6 mt-12 btn-sm btn btn-outline btn-warning'>Reset Password</button>
+                            </td>
+                            <td>
+                                <button className='mt-12 btn-sm btn btn-outline btn-error'>Deactivate</button>
+                            </td>
+                        </tr>
+                    </tbody></table>
             </div>
 
-        </div>
+        </div >
     )
 }
 
